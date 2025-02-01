8048e3fe518977294f0c61e8ea6051cedbf0982f
Javier Murgoitio Esandi <murgoiti@usc.edu>
2025-02-01 10:56:34 -0800

    uploading

---
+++ inverse_problems/HeatEquation_SingleSource_SamplingPosterior/sampling_posterior.py
@@ -0,0 +1,120 @@
+import numpy as np
+import argparse
+import os
+from solver_funcs import F_operator_HeatEquation
+import matplotlib.pyplot as plt
+from utils import noise_addition_image
+from posterior_funcs import Posterior, MuVector
+
+parser = argparse.ArgumentParser(description="Sampling the posterior")
+# parser.add_argument("--sigma_percentage", type=float, default=1.0)
+# parser.add_argument("--directory", type=str, default="results")
+parser.add_argument("--idx", type=str, default="results")
+parser.add_argument("--sigma", type=float, default=0.1)
+parser.add_argument("--epsilon", type=float, default=0.01)
+parser.add_argument("--nsamples", type=int, default=1000)
+args = parser.parse_args()
+
+idx = args.idx
+
+directory = f"results_{int(args.sigma*100)}percentnoise_{idx}"
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+x_vect = MuVector()
+_ = x_vect.sample_vector()
+x_vect.vector = np.load(
+    f"results_{int(args.sigma*100)}percentnoise_{idx}" + "/x_vect.npy"
+)
+
+x, y = F_operator_HeatEquation(x_vect.get_vector())
+meas_y = noise_addition_image(y, noise_level=args.sigma)
+np.save(directory + "/meas_y.npy", meas_y)
+np.save(directory + "/x_vect.npy", x_vect.get_vector())
+
+fig, ax = plt.subplots(1, 2, figsize=(10, 4))
+
+cb = ax[0].imshow(x[:, :])
+plt.colorbar(cb, ax=ax[0])
+cb = ax[1].imshow(meas_y[:, :])
+plt.colorbar(cb, ax=ax[1])
+
+plt.savefig(directory + "/x_and_meas_y.png")
+plt.close()
+
+## Sampling the posterior
+
+sigma = np.load(directory + "/sigma.npy")
+meas_y = np.load(directory + "/meas_y.npy")
+
+x_vect_chain = []
+logpost = []
+x_chain = []
+
+posterior = Posterior(sigma)
+x_vect = MuVector()
+_ = x_vect.sample_vector()
+x_vect_cur = x_vect.get_vector()
+
+
+# mu_cur = np.load(directory + f"/chain_{idx}.npy")[-1, :]
+# mu_vect.update_vector(mu_cur)
+# chain_ = np.load(directory + f"/chain_{idx}.npy")
+# logpost_ = np.load(directory + f"/logpost_{idx}.npy")
+# mu_chain_ = np.load(directory + f"/mu_chain_{idx}.npy")
+# for i in range(len(chain_)):
+#     chain.append(chain_[i])
+#     logpost.append(logpost_[i])
+#     mu_chain.append(mu_chain_[i])
+
+n_steps = args.nsamples
+# The following are the sampling parameters for noise as 100% of the maximum value of the image
+epsilon = args.epsilon
+factors = [1, 1, 1]
+np.save(directory + "/epsilon.npy", epsilon)
+np.save(directory + "/factors.npy", np.array(factors))
+# epsilon = np.load(directory + "/epsilon.npy")
+# factors = np.load(directory + "/factors.npy")
+
+n_accepted = 0
+
+x_cur, y_cur = F_operator_HeatEquation(x_vect_cur)
+p_cur = posterior.logpostprob(x_vect_cur, y_cur, meas_y)
+x_vect_chain.append(x_vect_cur)
+logpost.append(p_cur)
+x_chain.append(x_cur)
+
+for i in range(n_steps):
+    x_vect_prop = x_vect_cur + x_vect.get_step(epsilon, factors)
+    x_vect.update_vector(x_vect_prop)
+    x_prop, y_prop = F_operator_HeatEquation(x_vect.get_vector())
+    p_prop = posterior.logpostprob(x_vect_prop, y_prop, meas_y)
+    alpha = min(1, np.exp(p_prop - p_cur))
+    u = np.random.uniform()
+    if i % 10 == 0:
+        print(f"alpha: {alpha}; u: {u}, p_prop: {p_prop}, p_cur: {p_cur}")
+    if u < alpha:
+        x_vect_cur = x_vect_prop
+        p_cur = p_prop
+        x_cur = x_prop
+        n_accepted += 1
+
+    x_vect_chain.append(x_vect_cur)
+    logpost.append(p_cur)
+    x_chain.append(x_cur)
+
+    if i % 10 == 0:
+        print(f"Iteration: {i}; Acceptance rate: {n_accepted/(i+1)}")
+
+    if i % 100 == 0:
+        np.save(directory + f"/x_vect_chain_{idx}.npy", np.array(x_vect_chain))
+        np.save(directory + f"/x_chain_{idx}.npy", np.array(x_chain))
+        np.save(directory + f"/logpost_{idx}.npy", np.array(logpost))
+
+
+x_chain = np.array(x_chain)
+x_vect_chain = np.array(x_vect_chain)
+np.save(directory + f"/x_chain_{idx}.npy", x_chain)
+np.save(directory + f"/x_vect_chain_{idx}.npy", x_vect_chain)
+np.save(directory + f"/acceptance_rate_{idx}.npy", n_accepted / n_steps)
---
+++ inverse_problems/HeatEquation_SingleSource_SamplingPosterior/solver_funcs.py
@@ -0,0 +1,96 @@
+# Import all the functions from DOLFIN (computational backend of the FEniCS project)
+from fenics import *
+from dolfin import *
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
+
+
+def interpolate_function(func, function_space):
+    x = function_space.tabulate_dof_coordinates()[:, 0]
+    y = function_space.tabulate_dof_coordinates()[:, 1]
+    z = func.vector().get_local()
+    xi = np.linspace(0.0, 1.0, 64)  # New x-coordinates
+    yi = np.linspace(0.0, 1.0, 64)  # New y-coordinates
+    xi, yi = np.meshgrid(xi, yi)  # Create a 2D grid
+    zi = griddata(
+        (x, y), z, (xi, yi), method="cubic"
+    )  # Use 'cubic', 'linear', or 'nearest'
+    return zi
+
+
+def F_operator_HeatEquation(init_cond):
+    # 20 is number of intervals Omega is divided into
+    mesh = UnitSquareMesh(64, 64)
+    # here interval is a FEniCS builtin representing a single interval
+    elem = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
+    W = FunctionSpace(mesh, elem)
+
+    kappa = Constant(0.25)  # physical material property
+    S = Constant(0.0)  # source term
+
+    dt = Constant(0.01)  # time step
+    nb_t = 6  # number of time step - loop
+
+    v = TestFunction(W)  # the test function
+    # the TrialFunction is basically a symbol representing the unknown
+    u = TrialFunction(W)
+    u_old = Function(W)  # Solution at previous time step. Initialized to zero.
+
+    a1 = init_cond[0]
+    b1 = init_cond[1]
+    c1 = init_cond[2]
+    # d1 = init_cond[3]
+
+    P = FunctionSpace(mesh, "Lagrange", 1)
+
+    class Kappa_dom(UserExpression):
+        def __init__(self, a, b, c, **kwargs):
+            super().__init__(**kwargs)
+            self.x_c = a
+            self.y_c = b
+            self.radius = c
+            # self.temp = d
+
+        def eval(self, value, x):
+            if (x[0] - self.x_c) ** 2 + (x[1] - self.y_c) ** 2 < self.radius**2:
+                value[0] = 1.0
+            else:
+                value[0] = 0.0
+
+        def value_shape(self):
+            return ()
+
+    kappa_init = Kappa_dom(a1, b1, c1, degree=0)
+    u_old = interpolate(kappa_init, P)
+
+    # plot(u_old)
+
+    a = (u * v) / dt * dx + kappa * dot(
+        grad(u), grad(v)
+    ) * dx  # left hand side of our equation
+    L = (u_old * v) / dt * dx + S * v * dx  # right hand side of our equation
+
+    uh = Function(W)  # place to store the solution
+
+    A = assemble(a)
+    # for bc in bcs:
+    #     bc.apply(A)
+
+    solver = LUSolver(A)
+    i = 0
+    for t in range(nb_t):
+        b = assemble(L)  # Reassemble L on every time step
+        # for bc in bcs:  # RE-apply bc to b
+        #     bc.apply(b)
+        solver.solve(uh.vector(), b)  # Solve on given time step
+        assign(u_old, uh)
+
+        if i == 0:
+            u_init = interpolate_function(uh, P)
+        elif i == 5:
+            u_end = interpolate_function(uh, P)
+
+        i += 1
+
+    return u_init, u_end
